1ced373d75aafc1afe0cc28672438c247bba368b
François Honoré <francois.honore@acsone.eu>
2023-01-17 16:21:09 +0100

    [ADD] invader_payment_adyen_klarna_capture: capture payment [ADD] invader_payment_adyen_klarna_capture_delivery: capture payment when picking OUT is validated [UPD] Some changes/improvements on adyen_payment to know which payment method is selected (because it could be another klarna who doesn't need capture)

--- invader_payment_adyen/services/payment_adyen.py
+++ invader_payment_adyen/services/payment_adyen.py
@@ -481,11 +481,25 @@ class PaymentServiceAdyen(AbstractComponent):
         """
         Hook to be able to enrich transaction with response
         additionalData
-        :param vals:
-        :param response:
-        :return:
+        Payment result can contains payment method brand
         """
-        return {}
+        res = {}
+        if response.message.get("action", {}).get("paymentMethodType"):
+            payment_method = response.message.get("action", {}).get(
+                "paymentMethodType"
+            )
+            res.update({"adyen_payment_method": payment_method})
+        if response.message.get("paymentMethod", {}).get("type"):
+            payment_method = response.message.get("paymentMethod", {}).get(
+                "type"
+            )
+            res.update({"adyen_payment_method": payment_method})
+        if response.message.get("paymentMethod", {}).get("brand"):
+            payment_method = response.message.get("paymentMethod", {}).get(
+                "brand"
+            )
+            res.update({"adyen_payment_method": payment_method})
+        return res
 
     def _update_transaction_with_response(self, transaction, response):
         """
@@ -499,7 +513,9 @@ class PaymentServiceAdyen(AbstractComponent):
         payment_data = response.message.get("paymentData")
         if payment_data:
             vals.update({"adyen_payment_data": payment_data})
-        psp_reference = response.message.get("pspReference")
+        psp_reference = response.message.get("pspReference") or getattr(
+            response, "psp", ""
+        )
         if psp_reference:
             vals.update({"acquirer_reference": psp_reference})
         result_code = response.message.get("resultCode")
